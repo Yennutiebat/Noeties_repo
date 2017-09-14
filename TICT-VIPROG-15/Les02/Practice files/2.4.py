f3bdec60c4a3e3594c57a4864ac57d34d52b89c4
Noetie <noetiebatnibi@gmail.com>
2017-09-14 19:06:12 +0200

    test

---
+++ TICT-VIPROG-15/Les02/Practice files/2.4.py
@@ -0,0 +1,4 @@
+uurloon=eval(input('Wat verdien je per uur: '))
+aantalUur=eval(input('Hoeveel uur heb je gewerkt: '))
+totaalbedrag=uurloon*aantalUur
+print(str(aantalUur)+' uur werken levert '+str(totaalbedrag)+' Euro op')
