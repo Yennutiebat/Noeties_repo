f368833460bf5341a517b51ccca1537d99ea95fd
Noetie <noetiebatnibi@gmail.com>
2017-09-15 12:51:54 +0200

    oefening2.1

---
+++ TICT-VIPROG-15/Les02/Practice files/2.1.py
@@ -0,0 +1,6 @@
+letters=('a','c','b','b','c','a','c','c','b')
+resultaat=letters.count('a')
+resultaat1=letters.count('b')
+resultaat2=letters.count('c')
+resultaatlijst=[resultaat,resultaat1,resultaat2]
+print(resultaatlijst)
