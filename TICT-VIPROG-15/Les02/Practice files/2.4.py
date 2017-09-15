ef49a386933bff35ebb00a62e135ff6d55445386
Noetie <noetiebatnibi@gmail.com>
2017-09-15 12:52:14 +0200

    oefening2.1

--- TICT-VIPROG-15/Les02/Practice files/2.4.py
+++ TICT-VIPROG-15/Les02/Practice files/2.4.py
@@ -2,3 +2,4 @@ uurloon=eval(input('Wat verdien je per uur: '))
 aantalUur=eval(input('Hoeveel uur heb je gewerkt: '))
 totaalbedrag=uurloon*aantalUur
 print(str(aantalUur)+' uur werken levert '+str(totaalbedrag)+' Euro op')
+print('IK WIL KAAS')
