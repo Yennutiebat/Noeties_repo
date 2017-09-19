1cd6d2f7f7985c22ca07f7bbd72c0ed4f279f130
Noetie <noetiebatnibi@gmail.com>
2017-09-19 10:44:59 +0200

    oefeningen2

---
+++ TICT-VIPROG-15/Les02/Practice files/2.2.py
@@ -0,0 +1,7 @@
+cijferICOR=8
+cijferPROG=7.5
+cijferCSN=7
+totaal=cijferCSN+cijferICOR+cijferPROG
+gemiddelde=totaal/3
+beloning=totaal*30
+print('Mijn cijfers(gemiddeld een'+str(gemiddelde)+') leveren een beloning van $'+str(beloning)+'op')
