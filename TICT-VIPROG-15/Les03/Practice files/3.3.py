fd98780d9194e73f30ffacbbf1cd0f8dfab78caa
Noetie <noetiebatnibi@gmail.com>
2017-09-19 10:48:00 +0200

    oefening2.1

---
+++ TICT-VIPROG-15/Les03/Practice files/3.3.py
@@ -0,0 +1,6 @@
+leeftijd = eval(input('Geef je leeftijd: '))
+paspoort = input('Nederlands paspoort: ')
+if leeftijd >= 18  and paspoort =='ja':
+        print('Gefeliciteerd, je mag stemmen')
+else:
+    print('Je mag niet stemmen')
