c839e332f80bb01e401896979a36933a76b233a4
Noetie <noetiebatnibi@gmail.com>
2017-09-19 10:46:05 +0200

    oefening3.1

---
+++ TICT-VIPROG-15/Les03/Practice files/3.1.py
@@ -0,0 +1,6 @@
+score = eval(input('geef je score: '))
+if score >15:
+    print('Gefeliciteerd!')
+    print('met een score van '+str(score)+' ben je geslaagd!')
+else:
+    print('je bent gezakt')
